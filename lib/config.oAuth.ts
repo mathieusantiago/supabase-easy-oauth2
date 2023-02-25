f47697c30908e10897204bd6b366785567f0d53d
mathieusantiago <mathieusantiago21@gmail.com>
2023-02-25 15:17:35 +0100

    delete key

--- lib/config.oAuth.ts
+++ lib/config.oAuth.ts
@@ -1,11 +1,8 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const configEnv= (url: string, key: string)=> {
-    const supabase: SupabaseClient = createClient(
-        url, //"https://wpxvwfcdmgcsvczglefh.supabase.co",
-        key, //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndweHZ3ZmNkbWdjc3ZjemdsZWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcyODQ5NTIsImV4cCI6MTk5Mjg2MDk1Mn0.n84d9xUJmuxdigF8aFstFW4DRVw1dBLDx4Lb4JorMts"
-      );
-      return supabase;
-}
+const configEnv = (url: string, key: string) => {
+  const supabase: SupabaseClient = createClient(url, key);
+  return supabase;
+};
 
 export default configEnv;
